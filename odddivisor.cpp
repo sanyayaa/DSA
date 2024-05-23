55c4218c8e73070e1c0bc83e9d1531daf77e9a53
Sanya <sanyayaatiwari@gmail.com>
2024-05-23 16:58:57 +0530

    This is a new commit for what I originally planned to be amended

---
+++ odddivisor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+# include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--){
+        long long n;
+        cin >> n;
+        while(n % 2 == 0)
+            n /= 2;
+            
+        if(n > 1)
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
